44bb03dea6bc7a3942c5dade6661dbb519322d99
Douglas Squirrel <ds@douglassquirrel.com>
2016-09-07 14:14:49 +0100

    parse command line

--- src/clibook-test.rkt
+++ src/clibook-test.rkt
@@ -2,4 +2,11 @@
 
 (require "pipeline.rkt")
 
-(make-pipe (list "hello world"))
+;(make-pipe (list "hello world"))
+
+(define requests
+    (command-line
+        #:args requests
+        requests))
+
+requests
