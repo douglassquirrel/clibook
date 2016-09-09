8ff5bedabd91a346050f54fa5e86a8d6de669785
Douglas Squirrel <ds@douglassquirrel.com>
2016-09-09 22:34:19 +0100

    separate sprintf-hash

--- src/result-to-string.rkt
+++ src/result-to-string.rkt
@@ -2,18 +2,13 @@
 
 (require json
          "hashes.rkt"
+         "sprintf-hash.rkt"
          "url-result.rkt")
 
 (provide result-to-string)
 
 (define (remove-version-prefix path)
     (regexp-replace #rx"^v[^/]*/" path ""))
-
-(define (replace-hash-value h key s)
-    (string-replace s (format "%~a%" key) (hash-ref h key)))
-
-(define (sprintf-hash form h)
-    (foldl ((curry replace-hash-value) h) form (hash-keys h)))
 
 (define (result-to-string r) 
     (define path (remove-version-prefix (url-result-path r)))
